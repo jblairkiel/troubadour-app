2bc6667a59040789f0eca6c085d0551d70fd8c70
James Kiel <jblairkiel@gmail.com>
2022-07-02 15:51:43 -0500

    still working on responsive grid results

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 
 
 import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import useFetch from "./src/hooks/useFetch"
 import Search from "./src/component/Search";
 import TroubadourNavbar from './src/component/TroubadourNavbar'
@@ -8,54 +11,60 @@ import {Form, Card, TextInput} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-export default function App() {
-  const { data, setData } = useFetch();
-  const dummyData = {
-          "albums": [
-              {
-                  "spotify_id": "6vr32hctMBvABv8wlVd4iX",
-                  "images": [
-                      {
-                          "height": 640,
-                          "url": "https://i.scdn.co/image/ab67616d0000b2739144f24deb01def6346704f5",
-                          "width": 640
-                      },
-                      {
-                          "height": 300,
-                          "url": "https://i.scdn.co/image/ab67616d00001e029144f24deb01def6346704f5",
-                          "width": 300
-                      }
-                    ],
-                  "name": "Metallica"
-              }
-            ],
-          "artists": [],
-          "genres": [],
-          "tracks": [],
+function SearchScreen() {
+
+	const { data, setData } = useFetch();
+
+	const handleSubmit = (e) => {
+    	if (e.code === 'Enter') e.preventDefault();
+  	};
+  	const checkKeyDown = (e) => {
+    	if (e.code === 'Enter') e.preventDefault();
+  	};
+
+	return (
+		<div>
+			<Card style={{inline: 1, alignItems: "left", justifyContent: "left", padding: '5%'}}>
+			<Form onSubmit={(e)=> handleSubmit(e)}  onKeyDown={(e) => checkKeyDown(e)} >
+				<Form.Control
+				type="text"
+				placeholder="Search Spotify"
+				value={data.slug}
+				onChange={(e) => setData({ ...data, slug: e.target.value })}
+				/>
+			</Form>
+			
+			{Object.keys(data.results).length > 0 ? <Search searchQuery={data.results.data} /> : null } 
+			{/* <Search searchQuery={dummyData} />} */}
+			</Card>
+		</div>
+	);
+}
+
+function Root() {
+
+	const Tab = createBottomTabNavigator();
+
+	return (
+	  <Tab.Navigator>
+		<Tab.Screen name="Search" component={SearchScreen} />
+	  </Tab.Navigator>
+	);
   }
 
-  const handleSubmit = (e) => {
-    if (e.code === 'Enter') e.preventDefault();
-  };
-  const checkKeyDown = (e) => {
-    if (e.code === 'Enter') e.preventDefault();
-  };
-  return (
-	<div>
-		<TroubadourNavbar></TroubadourNavbar>
-		<Card style={{inline: 1, alignItems: "left", justifyContent: "left", padding: '5%'}}>
-		<Form onSubmit={(e)=> handleSubmit(e)}  onKeyDown={(e) => checkKeyDown(e)} >
-			<Form.Control
-			type="text"
-			placeholder="Search Spotify"
-			value={data.slug}
-			onChange={(e) => setData({ ...data, slug: e.target.value })}
-			/>
-		</Form>
-		
-		{Object.keys(data.results).length > 0 ? <Search searchQuery={data.results.data} /> : null } 
-		{/* <Search searchQuery={dummyData} />} */}
-		</Card>
-	</div>
-  );
+export default function App() {
+
+	const Stack = createNativeStackNavigator();
+
+	return (
+			<NavigationContainer>
+				<Stack.Navigator>
+					<Stack.Screen
+					name="Root"
+					component={Root}
+					options={{ headerShown: false }}
+					/>
+				</Stack.Navigator>
+			</NavigationContainer>
+	);
 }
--- src/component/SearchQuery.js
+++ src/component/SearchQuery.js
@@ -5,6 +5,11 @@ import React from "react";
 import {Card, ListGroup} from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
 import Figure from 'react-bootstrap/Figure';
+import {
+    Row,
+    Col,
+    Container
+} from 'react-bootstrap'
 
 export default function SearchQuery({ searchQuery}) {
 
@@ -15,59 +20,125 @@ export default function SearchQuery({ searchQuery}) {
 	// 		display: "inline-block"
 	// 	},
 	//   });
+	let colCount = 5
+	let mdVar = 6
 
+	//Index is needed to keep track of the current element that we are one.
+	let index = 0
+
+	const Item = props => {
+		
+		return (
+			<Card>
+				{props.images.length > 0 ? 
+				<Card.Img variant='top' style={{ width: '18rem' }} src={props.images[0].url} />  : null
+				}
+				<Card.Body>
+					<Card.Title>{props.name}</Card.Title>
+				</Card.Body>
+			</Card>
+		)
+	}
+	
+	const GridSystem = ({ colCount, children, searchTitle }) => {
+		return (
+			<Container className='Container'>
+				{
+					buildResults(colCount, children, searchTitle)
+				}
+			</Container>
+		);
+	}
+
+	const buildResults = (colCount, searchQuery, searchTitle) => {
+		let rowCount = Math.floor(searchQuery.length / colCount) + 1
+		let results = []
+		results.push(buildGrid(searchTitle, searchQuery, colCount, rowCount))
+		return(
+			results
+		)
+	}
+	//This is the driver function for building the grid system.
+	const buildGrid = (titleName, titleData, colCount, rowCount) => {
+		let header = (
+			<h2>{titleName}</h2>
+		)
+		let queryResults = renderRows(titleData, colCount, rowCount)
+
+		return (
+			<div >
+				{header}
+				{queryResults}
+			</div>
+		)
+	}
+	//Returns For example, we can have a row with 2 columns inside it.
+	const renderRows = (searchQuery, colCount, rowCount) => {
+
+		let rows = []
+		let index = 0
+		for(let row = 0; row < rowCount; row++) {
+			let renderResults = renderCols(searchQuery, colCount, rowCount, index)
+
+			rows.push(
+				<Row className='Row'>
+					{
+						renderResults.cols
+					}
+				</Row>
+			)
+			index = renderResults.index
+		}
+		let container = (
+			<Container>
+				{rows}
+			</Container>
+		)
+		return container
+	}
+
+	//Returns an array of columns with the children inside.
+	const renderCols = (queryResults, colCount, rowCount, index) => {
+        let cols = []
+        
+        //If you want to add more bootstrap breakpoints you can pass them as props here.
+        for(let col = 0; col < colCount; col++) {
+            if(index < queryResults.length) {
+                cols.push(
+                    <Col className='Col' md={mdVar} key={queryResults[index].spotify_id}>
+						{queryResults[index]}
+                    </Col>
+                )
+                index++
+            }
+        }
+        
+        return {index, cols}
+    }
 
 	return (
-		<Card>	
-			<h2>Albums</h2>
-			<ListGroup>
-				{
-					searchQuery.albums.map(function(d, idx){
-						return (
-						<ListGroup.Item key={idx}>
-							<Card>
-								{d.images.length > 0 ? 
-								<Figure>
-									<Figure.Image variant='top' width={50} height={50} src={d.images[0].url} />
-								</Figure>  : null
-								}
-								<Card.Title>{d.name}</Card.Title>
-							</Card>
-						</ListGroup.Item>
-						)
-					  })
-					
-				}
-			</ListGroup>
 
-			{/* 
-			Object.entries().map(([key, value]) => (
-						<ListGroup.Item key={key}>{value.name}</ListGroup.Item>
-					));	
-			<SectionList 
-			sections={[
-				{title: 'Albums', data: searchQuery.albums},
-				{title: 'Artists', data: searchQuery.artists},
-				{title: 'Genres', data: searchQuery.genres},
-				{title: 'Tracks', data: searchQuery.tracks},
-			]}
-			//renderItem={({ item }) => <Item title={item} />}
-
-			renderItem={({item})=>(
-				<View  style={[s.card]}>
-					<View style={[s.cardBody]}>
-						{item.images.length > 0 ? <Image style={styles.tinyLogo} source={{uri: item.images[0].url}} /> : null}
-						<Text style={[s.text]}>{item.name}</Text>
-					</View>
-				</View>
-			)}
-			//renderSectionHeader={({ section: { title } }) => <Text style={styles.header}>{title}</Text>}
-			
-			renderSectionHeader={({section})=>(
-				<Text style={[s.textPrimary]}>{section.title}</Text>
-			  )}
-			keyExtractor={(item, index) => item.spotify_id}
-			/> */}
-		</Card>
+		<Container>
+			<GridSystem colCount={2} md={mdVar} searchTitle={'Albums'}>
+			{ searchQuery.albums.length > 0 
+			? searchQuery.albums.map(item => <Item key={item.spotify_id} id={item.spotify_id} title={item.name} />) 
+			: [<p>No tracks are found.</p>] }
+			</GridSystem>
+			{/* <GridSystem colCount={2} md={6} searchTitle={'Artists'}>
+			{ searchQuery.artists.length > 0 
+			? searchQuery.artists.map(item => <Item key={item.spotify_id} id={item.spotify_id} title={item.name} />) 
+			: [<p>No tracks are found.</p>] }
+			</GridSystem>
+			<GridSystem colCount={2} md={6} searchTitle={'Genres'}>
+			{ searchQuery.genres.length > 0 
+			? searchQuery.genres.map(item => <Item key={item.spotify_id} id={item.spotify_id} title={item.name} />) 
+			: [<p>No tracks are found.</p>] }
+			</GridSystem>
+			<GridSystem colCount={2} md={6} searchTitle={'Tracks'}>
+			{ searchQuery.tracks.length > 0 
+			? searchQuery.tracks.map(item => <Item key={item.spotify_id} id={item.spotify_id} title={item.name} />) 
+			: [<p>No tracks are found.</p>] }
+			</GridSystem> */}
+		</Container>
 	)
 }
