673a2b5e00871105c4f967496e376b0ff3cf79a2
James Kiel <jblairkiel@gmail.com>
2022-07-07 23:04:46 -0500

    multiple tabs screens with auth

--- App.js
+++ App.js
@@ -1,18 +1,115 @@
 
 
-import React from "react";
+import {React, useEffect, useState} from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import useFetch from "./src/hooks/useFetch"
+import getUserData from "./src/hooks/getUserData"
 import Search from "./src/component/Search";
 import {ScrollView, View} from 'react-native';
 import TroubadourNavbar from './src/component/TroubadourNavbar'
-import {Form} from 'react-bootstrap';
+import {Form, Button} from 'react-bootstrap';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+function HomeScreen({navigation}) {
+	const CLIENT_ID = "a9fc8ed15eeb4790b184ab648dd4ebf5"
+	const REDIRECT_URI = "http://localhost:19006"
+	const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+	const RESPONSE_TYPE = "token"
 
-function SearchScreen() {
+	const [token, setToken] = useState("")
+	const [hash, setHash] = useState("")
+	const [userData, setUserData] = useState([])
+
+	const getUserData = async (e) => {
+		const {data} = await axios.get("https://api.spotify.com/v1/me", {
+			headers: {
+				Authorization: `Bearer ${e}`
+			}
+		})
+	
+		setUserData(data)
+	}
+	
+	  
+    useEffect(() => {
+        const hash = window.location.hash
+        let token = window.localStorage.getItem("token")
+
+        if (!token && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+            window.location.hash = ""
+            window.localStorage.setItem("token", token)
+        }
+
+        setToken(token)
+		if (token) {
+			getUserData(token)
+		}
+
+    }, [])
+
+	const logout = () => {
+        setToken("")
+        window.localStorage.removeItem("token")
+    }
+
+	const renderUserData = () => {
+		return (
+			<div>
+				{ userData != [] ? <h3>{userData.display_name}</h3> : <div>No Data</div>}
+				
+			</div>
+		)
+	}
+
+	return (
+		<View style={{flex:1,  alignItems: "left", justifyContent: "left", padding: '1%'}}>
+
+			{!token ?
+				<Button 
+				variant="primary"
+				href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+				>Login</Button>
+			: 
+				<Button 
+				variant="secondary"
+				onClick={logout}>Log Out</Button>
+			}
+			{renderUserData()}
+			
+			
+		</View>
+	);
+}
+
+function MyPreferencesScreen({navigation}) {
+
+	const { data, setData } = useFetch();
+
+
+	return (
+		<View style={{flex:1,  alignItems: "left", justifyContent: "left", padding: '1%'}}>
+
+			<Button 
+			variant="primary" 
+			style={{"float": "right"}}
+			onClick={() => navigation.push('PreferencesSearchScreen')}
+			> Add</Button>
+
+			{Object.keys(data.results).length > 0 ?
+			<ScrollView contentContainerStyle={{ flexGrow: 1 }}> 
+				<Search searchQuery={data.results.data} /> 
+			</ScrollView>
+			: null } 
+		</View>
+	);
+}
+
+function SearchScreen({navigation}) {
 
 	const { data, setData } = useFetch();
 
@@ -50,7 +147,8 @@ function Root() {
 
 	return (
 	  <Tab.Navigator>
-		<Tab.Screen name="Search" component={SearchScreen} />
+	  	<Tab.Screen name="Home" component={HomeScreen} />
+		<Tab.Screen name="My Preferences" component={MyPreferencesScreen} />
 	  </Tab.Navigator>
 	);
   }
@@ -67,6 +165,7 @@ export default function App() {
 					component={Root}
 					options={{ headerShown: false }}
 					/>
+					<Stack.Screen name="PreferencesSearchScreen" component={SearchScreen} />
 				</Stack.Navigator>
 			</NavigationContainer>
 	);
